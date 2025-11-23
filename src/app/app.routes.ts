7929a6e73cdb684cd81c928ecaa30e6b4947ca33
Sandeshth148 <sandeshth582@gmail.com>
2025-11-23 10:51:00 +0530

    Fix dark theme variables and integrate Streaks MFE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
 export const routes: Routes = [
   {
@@ -8,5 +9,12 @@ export const routes: Routes = [
   {
     path: 'weight-tracker',
     loadComponent: () => import('./features/weight-tracker/pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'streaks',
+    loadComponent: () => loadRemoteModule({
+      remoteName: 'fitlog-streaks',
+      exposedModule: './Component'
+    }).then(m => m.StreaksComponent)
   }
 ];
